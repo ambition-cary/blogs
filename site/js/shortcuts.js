a010827340e9aa55bae72b901ab2dce15a2dcee6
LiuKai <15735655371@163.com>
2024-09-24 21:14:55 +0800

    删除site

--- site/js/shortcuts.js
+++
@@ -1,8 +0,0 @@
-// 绑定快捷键
-keyboard$.subscribe(function(key) {
-    if (key.mode === "global" && key.type === "x") {
-      /* Add custom keyboard handler here */
-    //   console.log(key)
-      key.claim() 
-    }
-})
